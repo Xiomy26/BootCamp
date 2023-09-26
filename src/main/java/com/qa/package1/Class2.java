5ae26d660e424f9d95bbc45a4a6d62652a6a9228
xiomy26 <xiomyalmanzar@live.com>
2023-09-26 12:01:34 -0400

    added an addition method

--- src/main/java/com/qa/package1/Class2.java
+++ src/main/java/com/qa/package1/Class2.java
@@ -1,5 +1,9 @@
 package com.qa.package1;
 
 public class Class2 {
-
+ 
+	public static void main (String[]args) {
+		
+	System.out.println("the Grass is green");
+}
 }
--- src/main/java/com/qa/package1/Class3.java
+++ src/main/java/com/qa/package1/Class3.java
@@ -4,7 +4,11 @@ public class Class3 {
 
 	public static void main(String[] args) {
 		System.out.println("Life is hard");
-
+		addition () ;
+	}
+	public static void addition () {
+		int i= 2, j=3;
+		System.out.println(i+j);
 	}
 
 }
